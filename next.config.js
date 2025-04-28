9169fbc2c77bdaad6c3f6ab2650f4b29ab24cbe2
i9-9 <yvan.vrs@gmail.com>
2025-04-28 17:32:03 -0300

    feat: configura basePath y assetPrefix para exportación estática en /goyena

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/goyena',
+  assetPrefix: '/goyena/',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+}
+
+export default nextConfig 
