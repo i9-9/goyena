f8e425e408d7530c87ba0d5c49b1526eb588eec6
i9-9 <yvan.vrs@gmail.com>
2025-04-29 12:22:52 -0300

    Corregir configuración de Next.js para despliegue en Vercel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/goyena',
-  assetPrefix: '/goyena/',
   images: {
     unoptimized: true,
+    domains: ['images.ctfassets.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        pathname: '**',
+      },
+    ],
   },
   trailingSlash: true,
 }
